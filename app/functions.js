d2f8dde71a690ed41950f0c42d0410151c2c944a
Sanjay Dhakad <sanjaydhakad17@users.noreply.github.com>
2016-02-18 00:52:50 +0530

    Update function.js
    Update function.js

--- app/functions.js
+++ app/functions.js
@@ -2,26 +2,48 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+	return fn.apply(null, arr);
   },
 
   speak : function(fn, obj) {
-
+	return fn.call(obj)
   },
 
   functionFunction : function(str) {
-
+	return function(arg) {
+      return str + ', ' + arg;
+    };
   },
 
   makeClosures : function(arr, fn) {
+	var ret = [];
 
+    var makeFn = function(val) {
+      return function() { return fn(val); };
+    };
+
+    for (var i = 0, len = arr.length; i < len; i++) {
+      ret.push(makeFn(arr[i]));
+    }
+
+    return ret;
   },
 
   partial : function(fn, str1, str2) {
+    return function(str3) {
+      return fn.call(null, str1, str2, str3);
+    };
 
   },
 
   useArguments : function() {
+    var sum = 0;
+
+    for (var i = 0, len = arguments.length; i < len; i++) {
+      sum += arguments[i];
+    }
+
+    return sum;
 
   },
 
